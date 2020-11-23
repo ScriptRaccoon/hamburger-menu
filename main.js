c324f0d54f6fae3702859b69a026e3b35eeb3483
Script Raccoon <javascript.martin@gmail.com>
2020-11-23 23:52:01 +0100

    scroll to sections

--- main.js
+++ main.js
@@ -13,5 +13,6 @@ function clickMenuItem() {
     $(".hamburger").removeClass("cross");
     $(".menu").removeClass("menuOpen");
     const target = $(this).attr("data-item");
-    $("#status").text(`You clicked ${target}`);
+    const offset = $(`.${target}`).offset().top;
+    $("html, body").animate({ scrollTop: offset }, "slow");
 }
